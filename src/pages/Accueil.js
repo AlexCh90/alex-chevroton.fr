e753879be087fd8ba0f80bd88143a44d3a007c23
Alex Chevroton <alex.chevroton@protonmail.com>
2024-03-14 11:19:24 +0100

    14/03/2024 - 11 h 19

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,8 +1,18 @@
 import "../assets/styles/accueil.scss"
+import chat1 from "../assets/images/chat1.jpg"
+import chat2 from "../assets/images/chat2.jpg"
 
 function Accueil (){
     return (
-        <div id="fond" className="background background-image fixed-top">
+        <div id="carouselExampleSlidesOnly" className="carousel slide fondentete" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active" data-bs-interval="4000">
+                    <img src={chat1} className="carouselimage" alt="chat1"/>
+                </div>
+                <div className="carousel-item" data-bs-interval="2000">
+                    <img src={chat2} className="carouselimage" alt="chat2"/>
+                </div>
+            </div>
         </div>
         )
 }
